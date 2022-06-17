da41e678745a5f52a708accefb60c13df989a6e1
ming <850209126@qq.com>
2022-06-17 18:24:42 +0800

    0617更新 1. 完成了编辑提交修改 2. 完成了置顶后自动刷新页面 3. 详情页加入了一些信息，但感觉后端给的数据有点奇怪，详情页仅展示数据

--- src/views/itemList/itemDetail/index.vue
+++ src/views/itemList/itemDetail/index.vue
@@ -2,41 +2,42 @@
   <div>
     <div class="headContainer clearfix">
       <div class="headTitle">
-        真北公益计划
+        <span>{{ detailInfo[0] ? detailInfo[0].title : "default" }}</span>
         <div class="headBtn">
           <el-button size="default">删除</el-button>
           <el-button type="primary" size="default">编辑</el-button>
         </div>
       </div>
       <div class="headText">
-        <span>所属领域：基础教育</span>
-        <span>发布时间：2021-07-01 12:00</span>
+        <span
+          >所属领域：{{
+            detailInfo[0] ? detailInfo[0].typeName : "default"
+          }}</span
+        >
+        <span
+          >发布时间：{{
+            detailInfo[0] ? detailInfo[0].createdTime : "default"
+          }}</span
+        >
       </div>
     </div>
     <div class="app-container">
       <div class="mainContainer">
         <div class="mainTitle">项目动态</div>
-        <el-table>
-          <el-table-column label="主标题"> </el-table-column>
-          <el-table-column label="内容类别"> </el-table-column>
-          <el-table-column label="关联项目"> </el-table-column>
+        <el-table :data="detailInfo">
+          <el-table-column label="主标题" prop="title"> </el-table-column>
+          <el-table-column label="内容类别" prop="typeName"> </el-table-column>
+          <el-table-column label="关联项目" prop="typeName"> </el-table-column>
           <el-table-column label="封面图片">
             <template v-slot="{ row }">
-              <img
-                :src="row.imgUrl"
-                alt=""
-                style="width: 120px; height: 80px"
-              />
+              <img :src="row.image" alt="" style="width: 120px; height: 80px" />
             </template>
           </el-table-column>
-          <el-table-column label="发布时间"> </el-table-column>
+          <el-table-column label="发布时间" prop="createdTime">
+          </el-table-column>
           <el-table-column label="操作">
             <template>
-              <el-button
-                type="text"
-                @click="$router.push({ name: 'ItemDetail' })"
-                >查看</el-button
-              >
+              <el-button type="text">查看</el-button>
               <el-button type="text" @click="dialogFormVisible = true"
                 >编辑</el-button
               >
@@ -51,7 +52,48 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      detailInfo: [],
+    };
+  },
+  methods: {
+    // 获取页面基础数据
+    async pageInit() {
+      let res = await this.$api.postProjectQueryById(
+        {},
+        {
+          params: {
+            projectId: this.$route.query.projectId,
+          },
+        }
+      );
+      if (res.code === 200) {
+        // 因为返回的数据type是数字，所以进行一些处理
+        let typeName;
+        switch (res.data.type + "") {
+          case "0":
+            typeName = "基础教育";
+            break;
+          case "1":
+            typeName = "职业成长";
+            break;
+          case "2":
+            typeName = "企业公益";
+            break;
+          default:
+            break;
+        }
+        res.data.typeName = typeName;
+        this.detailInfo.push(res.data);
+      }
+    },
+  },
+  created() {
+    this.pageInit();
+  },
+};
 </script>
 
 <style scoped>
